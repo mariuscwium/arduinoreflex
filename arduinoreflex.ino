a52138794fb8d79b8322194d7afd5cb4b8b48240
Marius <marius.wium@redseed.com>
2019-04-21 16:57:00 +1200

    Step 3 - Adding another LED

--- arduinoreflex.ino
+++ arduinoreflex.ino
@@ -1,18 +1,38 @@
-const int ledPin = 9;
+const int startLedPin = 13;
+const int playerLedPin = 9;
 const int buttonPin = 8;
 
 int buttonState = 0;
+bool listenToButton = false;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(playerLedPin, OUTPUT);
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
+  
+  if(millis() > randomMillis()){
+    digitalWrite(startLedPin, HIGH);
+    listenToButton = true;
   }
+
+  if(listenToButton){
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH) {
+      digitalWrite(playerLedPin, HIGH);
+    } else {
+      digitalWrite(playerLedPin, LOW);
+    }
+  }
+
+}
+
+long unsigned randomMillis(){
+  unsigned randomN;
+  unsigned randomMillis;
+  randomSeed(analogRead(A0));
+  randomN = random(2,6);
+  randomMillis = randomN*1000;
+  return randomMillis;
 }
